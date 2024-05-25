6bbc7d5dd4e0a947b2e0d067138b2e4bb7502d72
chidumennamdi <kurtwanger40@gmail.com>
2024-05-25 08:27:34 +0100

    Commit

--- HttpRequest/MainView.swift
+++ HttpRequest/MainView.swift
@@ -6,3 +6,88 @@
 //
 
 import Foundation
+import SwiftUI
+
+var index = 0;
+
+struct TabItem: Identifiable {
+    var id = index + 1;
+    var name: String;
+}
+
+struct HistoryItem: Identifiable {
+    var id = UUID();
+    var name: String;
+}
+
+struct MainView: View {
+    
+    @State private var tabs: [TabItem] = [
+    TabItem(name: "POST history")
+    ]
+    
+    @State private var history: [HistoryItem] = [HistoryItem(name: "POST request")]
+    
+    @State var selectedHistory: String = "";
+        
+    var body: some View {
+        HStack {
+            
+            Section {
+                TabView(selection: $selectedHistory) {
+                    List(history) { historyItem in
+                        
+                        ForEach(1...18, id:\.self) { item in
+                            Text(historyItem.name).padding(.vertical, 5.0)
+                                .onTapGesture {
+                                }
+                        }
+                        
+                    }.tabItem {
+                        Text("History")
+                    }.tag(1)
+                    Text("Collection").tabItem { Text("Collections") }.tag(2)
+                }.frame(width: 180)
+            }.padding(.all, 4)
+            
+            Section {
+                VStack {
+                    HStack {
+                        
+                        ScrollView(.horizontal, showsIndicators: true) {
+                                HStack {
+                                    ForEach(tabs) { tab in
+                                        HStack {
+                                        Text("\(tab.name)")
+                                            Image(systemName: "xmark.circle.fill").foregroundStyle(Color.gray).onTapGesture(perform: {
+                                            tabs.remove(at: tab.id)
+                                        })
+                                        }.padding(6)
+                                            .border(Color.black, width: 1).cornerRadius(3.0)
+                                }
+                                
+                            }
+                        }.padding([.trailing, .top, .leading], 4.0)
+                        
+                        Image(systemName: "plus.circle.fill")
+                            .onTapGesture(perform: {
+                                tabs.append(TabItem(name: "Untitled request"))
+                            }).padding(.trailing, 4.0)
+
+                        
+                    }.padding(.all, 5)
+                    Divider()
+                    ScrollView {
+                        RequestUIView()
+                    }.padding(.all, 5)
+                    Spacer()
+                }
+            };
+        }
+    }
+}
+
+#Preview {
+    MainView()
+        
+}
--- HttpRequest/RequestUIView.swift
+++ HttpRequest/RequestUIView.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RequestUIView: View {
+    @State var url: String = ""
+    
+    let httpRequest = HttpService()
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Untitled POST request")
+                Spacer()
+            }
+            HStack {
+                TextField("Enter URL", text: $url)
+                Button(action: {
+                    httpRequest.makeRequest()
+                }, label: {
+                    Text("Send")
+                })
+            }
+            Spacer()
+            TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+                Text("Tab Content 1").tabItem { Text("Auth") }.tag(1)
+                Text("Tab Content 2").tabItem { Text("Body") }.tag(2)
+                Text("Tab Content 2").tabItem { Text("Headers") }.tag(3)
+                Text("Tab Content 2").tabItem { Text("Parameters") }.tag(4)
+            }
+            TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+                Text("Tab Content 1").tabItem { Text("Response") }.tag(1)
+                Text("Tab Content 2").tabItem { Text("Cookies") }.tag(2)
+            }
+
+        }
+        
+        
+    }
+}
+
+#Preview {
+    RequestUIView().frame(width: 600, height: 400)
+}
