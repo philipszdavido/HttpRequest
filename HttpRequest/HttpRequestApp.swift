fb726d338a1054595827f20d6c76ec64b90f3eae
chidumennamdi <kurtwanger40@gmail.com>
2024-05-25 11:58:53 +0100

    Commit

--- HttpRequest/HttpRequestApp.swift
+++ HttpRequest/HttpRequestApp.swift
@@ -13,6 +13,7 @@ struct HttpRequestApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
+            History.self
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
